c34d9ecc783ca0bfbf91f3fb55001eef300a0a3e
Tom Hockett <tom.hockett@gmail.com>
2026-02-20 19:58:04 -0700

    TC-26 Switch task store to SQLite with persistent storage setup in user directory (#13) (#13)

--- cmd/task/main.go
+++ cmd/task/main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/tomhockett/task-cli/cli"
 	"github.com/tomhockett/task-cli/task"
 )
 
 func main() {
-	store := task.NewInMemoryTaskStore()
+	// Get home directory and create ~/.task-cli/tasks.db path
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to get home directory:", err)
+		os.Exit(1)
+	}
+
+	dbDir := filepath.Join(home, ".task-cli")
+	// 0755 is Unix file permissions (rwxr-xr-x)
+	if err := os.MkdirAll(dbDir, 0755); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to create data directory:", err)
+		os.Exit(1)
+	}
+
+	dbPath := filepath.Join(dbDir, "tasks.db")
+	store, err := task.NewSQLiteStore(dbPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to open database:", err)
+		os.Exit(1)
+	}
+
 	c := cli.NewCLI(store, os.Stdout)
 
-	// os.Args[1:] slices off the program name
-	// os.Args[0] is always the binary name, so
-	// os.Args[1:] gives you just the user's arguments
 	if err := c.Run(os.Args[1:]); err != nil {
-		// Errors go to os.Stderr and exit with code 1
-		// the Unix convention for CLI tools
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
